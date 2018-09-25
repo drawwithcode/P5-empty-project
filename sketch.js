42eb3363387f36d6151427723abc91135fc51dab
iosonosempreio <tommaso.elli@gmail.com>
2018-09-25 14:11:32 +0200

    updated according to p5js toolbar

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+function preload(){
+  // put preload code here
+}
+
 function setup() {
-	// put setup code here
+  // put setup code here
 }
 
 function draw() {
-	// put drawing code here
+  // put drawing code here
 }
