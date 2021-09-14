6864e70d8371b597d68128cb96b76e298504a8f5
mikima <mikimauri@gmail.com>
2021-09-14 12:21:07 +0200

    Updated to p5 1.4.0 and VSCode structure

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
-function preload(){
+function preload() {
   // put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth, windowHeight);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
+  background("red");
 }
