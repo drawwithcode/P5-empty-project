91cf96f04244caa83e8f4bbcaca21edd7f978cc7
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-10-05 10:21:04 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 function preload() {
-  // put preload code here
+	// put preload code here
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+	createCanvas(windowWidth, windowHeight);
+	// put setup code here
+	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
+	textAlign(CENTER, CENTER);
+  textSize(16)
+	text(message, width/2, height/2);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+	// put drawing code here
 }
